41f3333fe1c0bb924238a537da0095d086dd2397
Lenny Truong <lenny@stanford.edu>
2018-03-08 16:20:09 -0800

    Initial commit, ported from magma

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bitvector',
+    version='0.1-alpha',
+    description='A BitVector class for Python',
+    scripts=[],
+    packages=[
+        "bitvector",
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6'
+)
