4f5ebbb77a11c55a870c91db80e60b9e806a04b2
Lenny Truong <lenny@stanford.edu>
2018-03-08 16:23:00 -0800

    Add numpy requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+
+install_requires = []
+extra_requires = {}
+for item in parse_requirements("requirements.txt", session=False):
+    req = str(item.req)
+    if item.markers is not None:
+        req += ";" + str(item.markers)
+    install_requires.append(req)
 
 setup(
     name='bitvector',
