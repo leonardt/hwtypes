a16801a7ca70ce3f021feb240a5132d65ecf2aac
Lenny Truong <lenny@stanford.edu>
2018-03-21 18:58:27 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for item in parse_requirements("requirements.txt", session=False):
 
 setup(
     name='bit_vector',
-    version='0.8-alpha',
+    version='0.9-alpha',
     description='A BitVector class for Python',
     scripts=[],
     packages=[
