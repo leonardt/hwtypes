1441b7c41d171cfb2c2d3c2d94d55a3cdc2279b7
Lenny Truong <lenny@stanford.edu>
2018-04-11 10:50:51 -0700

    Remove requires logic from setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup
-from pip.req import parse_requirements
+# from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 
-install_requires = []
-extra_requires = {}
-for item in parse_requirements("requirements.txt", session=False):
-    req = str(item.req)
-    if item.markers is not None:
-        req += ";" + str(item.markers)
-    install_requires.append(req)
+# install_requires = []
+# extra_requires = {}
+# for item in parse_requirements("requirements.txt", session=False):
+#     req = str(item.req)
+#     if item.markers is not None:
+#         req += ";" + str(item.markers)
+#     install_requires.append(req)
 
 setup(
     name='bit_vector',
     url='https://github.com/leonardt/bit_vector',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    version='0.20-alpha',
+    version='0.21-alpha',
     description='A BitVector class for Python',
     scripts=[],
     packages=[
         "bit_vector",
     ],
-    install_requires=install_requires,
+    install_requires=['numpy'],
     # python_requires='>=3.6'
 )
