86f9be8ad7f47c1609d8e816c7ccb40469478c55
Lenny Truong <lenny@stanford.edu>
2019-01-07 16:33:34 -0800

    Add tests for overflow

---
+++ tests/test_overflow.py
@@ -0,0 +1,20 @@
+from bit_vector import SIntVector as SV
+from bit_vector import overflow
+import random
+import pytest
+
+
+ovfl_params = []
+
+for i in range(0, 32):
+    n = random.randint(1, 32)
+    a = SV(random.randint(-(1 << (n - 1)), (1 << (n - 1)) - 1), n)
+    b = SV(random.randint(-(1 << (n - 1)), (1 << (n - 1)) - 1), n)
+    ovfl_params.append((a, b))
+
+
+@pytest.mark.parametrize("a,b", ovfl_params)
+def test_ovfl(a, b):
+    expected = ((a < 0) and (b < 0) and ((a + b) >= 0)) or \
+               ((a >= 0) and (b >= 0) and ((a + b) < 0))
+    assert overflow(a, b, a + b) == expected
