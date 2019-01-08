9d6731cd6a0852211f6c1093122bde35496d7d65
Lenny Truong <lenny@stanford.edu>
2019-01-07 16:19:21 -0800

    Add test for adc

---
+++ tests/test_adc.py
@@ -0,0 +1,20 @@
+from bit_vector import BitVector as BV
+import random
+import pytest
+
+
+adc_params = []
+
+for i in range(0, 32):
+    n = random.randint(0, 32)
+    a = BV(random.randint(0, (1 << n) - 1), n)
+    b = BV(random.randint(0, (1 << n) - 1), n)
+    c = BV(random.randint(0, 1), 1)
+    adc_params.append((a, b, c))
+
+
+@pytest.mark.parametrize("a,b,c", adc_params)
+def test_adc(a, b, c):
+    res, carry = a.adc(b, c)
+    assert res == a + b + c
+    assert carry == (a.zext(1) + b.zext(1) + c)[-1]
