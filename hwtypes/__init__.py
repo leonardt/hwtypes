7f2b30e87044fd86847306d2fce87c6e3f8834ec
Lenny Truong <lenny@stanford.edu>
2019-05-29 16:14:48 -0700

    Temporarily skip fp_vector import

--- hwtypes/__init__.py
+++ hwtypes/__init__.py
@@ -3,6 +3,4 @@ from .bit_vector_abc import *
 from .adt import *
 from .smt_bit_vector import *
 from .z3_bit_vector import *
-from .fp_vector_abc import *
-from .fp_vector import *
 from .modifiers import *
