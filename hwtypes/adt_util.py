f3b1f75ffd741401805063338f909cd3b6b1258d
Ross <rdaly525@stanford.edu>
2019-08-05 13:45:21 -0700

    added test for rebind_keep_modifiers

--- hwtypes/adt_util.py
+++ hwtypes/adt_util.py
@@ -2,12 +2,17 @@ from .adt_meta import BoundMeta
 from .bit_vector_abc import AbstractBitVectorMeta, AbstractBitVector
 
 from .util import _issubclass
-from hwtypes.modifiers import get_all_modifiers
+from hwtypes.modifiers import unwrap_modifier, wrap_modifier, is_modified
 
 def rebind_bitvector(
         adt,
         bv_type_0: AbstractBitVectorMeta,
-        bv_type_1: AbstractBitVectorMeta):
+        bv_type_1: AbstractBitVectorMeta,
+        keep_modifiers=False):
+    if keep_modifiers and is_modified(adt):
+        unmod, mods = unwrap_modifier(adt)
+        return wrap_modifier(rebind_bitvector(unmod,bv_type_0,bv_type_1,True),mods)
+
     if _issubclass(adt, bv_type_0):
         if adt.is_sized:
             return bv_type_1[adt.size]
@@ -16,22 +21,23 @@ def rebind_bitvector(
     elif isinstance(adt, BoundMeta):
         new_adt = adt
         for field in adt.fields:
-            new_field = rebind_bitvector(field, bv_type_0, bv_type_1)
+            new_field = rebind_bitvector(field, bv_type_0, bv_type_1,keep_modifiers)
             new_adt = new_adt.rebind(field, new_field)
         return new_adt
     else:
         return adt
 
+def rebind_keep_modifiers(adt, A, B):
+    if is_modified(adt):
+        unmod, mods = unwrap_modifier(adt)
+        return wrap_modifier(rebind_keep_modifiers(unmod,A,B),mods)
 
-def rebind_keep_modifiers(adt, A, B, rebind_sub_types=False):
-    if A is B or (rebind_sub_types and _issubclass(adt, B)):
-        for mod in get_all_modifiers(A):
-            B = mod(B)
+    if _issubclass(adt,A):
         return B
     elif isinstance(adt, BoundMeta):
         new_adt = adt
         for field in adt.fields:
-            new_field = rebind_keep_modifiers(field, A, B, rebind_sub_types)
+            new_field = rebind_keep_modifiers(field, A, B)
             new_adt = new_adt.rebind(field, new_field)
         return new_adt
     else:
